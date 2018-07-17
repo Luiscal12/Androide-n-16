862f6f8d5e8f65f08f827a89736bd3599674e2f1
Luiscal12 <41337343+Luiscal12@users.noreply.github.com>
2018-07-17 12:28:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
-const Discord = require('discord.js');
+onst Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Estoy listo!');
+    console.log('Estoy Listo!!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-      message.reply('pong');
-      }
+    	message.reply('pong');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
